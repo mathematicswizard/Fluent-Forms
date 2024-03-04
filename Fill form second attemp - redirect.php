4e5d5db7a1fbab893076c30185a9e2f6876ca6d2
mathematicswizard <89847404+mathematicswizard@users.noreply.github.com>
2024-03-05 00:05:45 +0200

    Update Fill form second attemp - redirect.php

--- Fill form second attemp - redirect.php
+++ Fill form second attemp - redirect.php
@@ -2,38 +2,21 @@
 
 // Function to check for form submission and redirect if needed
 function check_form_submission_and_redirect() {
-    // Echo a message to indicate that the function is being executed
- //   echo "Checking form submission and redirect...<br>";
     
- if (is_user_logged_in() && is_page(77617)) {
-     
-        // Echo a message to indicate that the page and user authentication conditions are met
-//        echo "Page ID 77617 detected, user is logged in.<br>";
+ if (is_user_logged_in() && is_page(page-number)) { //chagnge page-number to your liking, the page where the form is embedded
 
         $user_id = get_current_user_id();
-        $form_id = 44;
+        $form_id = form-number;  // change form_id to your liking
         
-        // Echo a message to indicate the user and form IDs being used for the query
-//        echo "User ID: " . $user_id . ", Form ID: " . $form_id . "<br>";
 
         $userEntries = \FluentForm\App\Models\Entry::where('form_id', $form_id)
                                                       ->where('user_id', $user_id)
                                                       ->get();
 
-        // Echo a message to indicate whether $userEntries is an array
-//        echo "Is userEntries an array? " . (is_array($userEntries) ? 'Yes' : 'No') . "<br>";
-
-        // Echo a message to indicate the number of entries retrieved
-//      echo "Number of entries retrieved: " . count($userEntries) . "<br>";
-
         $hasSubmittedForm = count($userEntries) > 0;
 
-        // Echo a message to indicate whether the user has submitted the form
-//        echo "User has submitted form: " . ($hasSubmittedForm ? 'Yes' : 'No') . "<br>";
-
-
         if ($hasSubmittedForm) {
-            wp_redirect(home_url('/webinar-first-year/'));  // Replace with the actual URL
+            wp_redirect(home_url('url'));  // Replace with the actual URL to redirect
             exit();
         }
         }
